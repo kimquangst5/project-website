497a00ca5b1dea600e4494d01502f47a1b0ace89
Kim Quang <kimquangst5@gmail.com>
2024-09-28 23:13:01 +0700

    28/08/2005

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,29 +16,48 @@ if (formChat) {
 
 // SEVER_RETURN_MESSAGE
 socket.on("SEVER_RETURN_MESSAGE", (data) => {
-	console.log(data)
 	const div = document.createElement('div');
-	div.classList.add('col-span-1')
+	div.classList.add('h-max')
+	div.classList.add('w-1/2')
+	div.classList.add('p-[5px]')
 	const formChat = document.querySelector('[form-chat]');
 	if (formChat) {
 		const userId = formChat.getAttribute('form-chat');
 		if (userId) {
 			if (userId == data.userId) {
+				div.classList.add('ml-auto')
 				div.innerHTML = `
-					<div class="text-right h-max flex justify-end my-[7px]"><div class="bg-[#F5F5F5] w-max rounded-[8px] p-[10px]">${data.content}</div></div>
+					<div class="ml-auto bg-[#F5F5F5] w-max rounded-[8px] p-[10px]">${data.content}</div>
 				`
+				
+
 			} else {
 				div.innerHTML = `
-					<div><div class="font-bold text-[14px]">${data.fullName}</div><div class="rounded-[8px] bg-[#F1556A] text-[white] w-max px-[10px] py-[4px]">${data.content}</div></div>
+					<div class="font-bold text-[14px]">${data.fullName}</div><div class="rounded-[8px] bg-[#F1556A] text-[white] w-max px-[10px] py-[4px]">${data.content}</div>
 `
 			}
-		}
-		const appendchild = formChat.querySelector('[appendchild]');
-		if (appendchild) {
+			const appendchild = formChat.querySelector('[appendchild]');
 			appendchild.appendChild(div)
+			if (userId == data.userId) {
+				const appendchild = formChat.querySelector('[appendchild]');
+				if (appendchild) {
+					console.log(appendchild.scrollTop)
+					console.log(appendchild.scrollHeight)
+					
+					appendchild.scrollTop = appendchild.scrollHeight
+
+				}
+			}
 		}
 	}
 	// body.appendChild(fullName)
 	// body.appendChild(content)
 });
 // SEVER_RETURN_MESSAGE
+const appendchild = formChat.querySelector('[appendchild]');
+if (appendchild) {
+	// console.log(appendchild.scrollTop)
+	// console.log(appendchild.scrollHeight)
+	appendchild.scrollTop = appendchild.scrollHeight
+
+}
